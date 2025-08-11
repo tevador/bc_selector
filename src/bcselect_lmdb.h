5489637d45497ba8b969a99093cc11c1b6c304b9
tevador <tevador@protonmail.com>
2025-08-11 20:21:17 +0200

    Fix lmdb usage fixes #1

--- src/bcselect_lmdb.h
+++ src/bcselect_lmdb.h
@@ -8,6 +8,10 @@ typedef struct bcsel_lmdb {
     const char* path;
     MDB_env* m_env;
     bool is_open;
+    MDB_dbi m_blocks;
+    MDB_dbi m_txs_pruned;
+    MDB_dbi m_tx_indices;
+    MDB_dbi m_block_info;
 } bcsel_lmdb;
 
 void bcselect_lmdb_create(bcsel_lmdb* self, const char* path);
